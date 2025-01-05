f7779589d3917c67f662554a81182d36615de222
rim967 <rimnazih03@gmail.com>
2025-01-05 21:17:58 +0100

    ...

--- Main.java
+++ Main.java
@@ -2,52 +2,12 @@ import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Color;
 
-/*public class Main {
-
-    public static void main(String[] argvs) {
-        Environnement env = new Environnement(42, 54);
-
-        // Create obstacles
-        env.generateRandomObstacles("medium");
-
-        // Create drone
-        Position dronePos = new Position(20, 20, 0);  // Drone within grid bounds
-        drone myDrone = new drone(101, 80.5, dronePos);
-        env.ajoutDrone(myDrone);
-
-        // Create target
-        Position targetPos = new Position(30, 14, 0);  // Target within grid bounds
-        env.ajoutDest(targetPos);
-
-        // Create and display GUI
-        JFrame frame = new JFrame("Drone Environment");
-        frame.setSize(1000, 800);  // Adjusted size for visibility
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        EnvironmentPanel panel = new EnvironmentPanel(env);
-        frame.add(panel);
-        frame.setVisible(true);
-
-        // Start drone navigation in a separate thread
-        new Thread(() -> {
-            myDrone.navigateTo(targetPos, env, panel);
-            System.out.println("Drone position : " + myDrone.getPosition() );
-
-        }).start();
-
-    }
-}*/
-
-
-import javax.swing.*;
-import java.awt.Graphics;
-import java.awt.Color;
 
 public class Main {
     public static void main(String[] argvs) {
         // Création de l'environnement
         Environnement env = new Environnement(42, 54);
-     env.generateRandomObstacles("easy");
+    
         // Création des 3 drones avec des positions différentes
         Position dronePos1 = new Position(5, 5, 0);
         Position dronePos2 = new Position(20, 20, 0);
@@ -73,7 +33,8 @@ public class Main {
         env.ajoutDest(targetPos3);
 
         
-       
+        //création des obstacles 
+        env.generateRandomObstacles("easy");
 
         // Création et affichage de l'interface graphique
         JFrame frame = new JFrame("Simulation Multi-Drones");
